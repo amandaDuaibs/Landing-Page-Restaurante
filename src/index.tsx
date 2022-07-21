280944e1e1b8c090d907ebad76bc1871091ec268
Your Name <amanda.sales@braskem.com>
2022-07-21 01:33:28 -0300

    inicio do curso (parte 2) - rotas

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import "./index.css";
-import Cardapio from "./pages/Cardapio";
+import Inicio from "pages/Inicio";
+import Cardapio from "pages/Cardapio";
+
+const componenteAtual= window.location.pathname === '/' ? <Inicio/> : <Cardapio/>;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cardapio />
+    {componenteAtual}
   </React.StrictMode>,
   document.getElementById('root')
 );
