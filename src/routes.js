4c91cab81bd033cbb298366536f435f111e18e17
Your Name <amanda.sales@braskem.com>
2022-07-21 21:26:58 -0300

    organizado

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,20 @@
 import Menu from 'Components/menu';
+import PaginaPadrao from 'Components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function AppRouter(){
     return(
-        <Router>
-            <Menu/>
-            <Routes>
-                <Route path='/' element={<Inicio/>}/>
-                <Route path='/cardapio' element={<Cardapio/>}/>
-            </Routes>
-        </Router>
+       <main>
+            <Router>
+                <Menu/>
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao/>}/>
+                    <Route index element={<Inicio/>}/>
+                    <Route path='cardapio' element={<Cardapio/>}/>  
+                </Routes>
+            </Router>
+       </main>
     );
 }
